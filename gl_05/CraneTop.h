fe4b4a58e9a260872f285ed68edff168611133a7
Maciej Kasprzyk <maciejkasprzyk98@gmail.com>
2020-01-30 23:53:14 +0100

    naprawiono ruch liny

--- gl_05/CraneTop.h
+++ gl_05/CraneTop.h
@@ -10,22 +10,6 @@ public:
 		float height = 14.0f;
 		float dbase = 0.5f; // okresla jak bardzo oddalone sa od siebie dwie pierwsze rury trzonu
 
-		//przeciwwagi
-		auto counterweight1 = std::unique_ptr<Cube>(new Cube(GREY));
-		counterweight1->scale(glm::vec3(1.4f, 0.17f, 0.4f));
-		counterweight1->move(glm::vec3(0.2f, 0.5f, 0.25f));
-
-		auto counterweight2 = std::unique_ptr<Cube>(new Cube(GREY));
-		counterweight2->scale(glm::vec3(1.4f, 0.17f, 0.4f));
-		counterweight2->move(glm::vec3(0.2f, 0.7f, 0.25f));
-
-		auto counterweight3 = std::unique_ptr<Cube>(new Cube(GREY));
-		counterweight3->scale(glm::vec3(1.4f, 0.17f, 0.4f));
-		counterweight3->move(glm::vec3(0.2f, 0.9f, 0.25f));
-
-		addObject(std::move(counterweight1));
-		addObject(std::move(counterweight2));
-		addObject(std::move(counterweight3));
 
 		// przyczep na line
 		auto box = std::unique_ptr<Cube>(new Cube(YELLOW));
@@ -112,8 +96,27 @@ public:
 			}
 		}
 
+		
+		//przeciwwagi
+		auto counterweight1 = std::unique_ptr<Cube>(new Cube(GREY));
+		counterweight1->scale(glm::vec3(1.4f, 0.17f, 0.4f));
+		counterweight1->move(glm::vec3(0.2f, 0.5f, 0.25f));
+
+		auto counterweight2 = std::unique_ptr<Cube>(new Cube(GREY));
+		counterweight2->scale(glm::vec3(1.4f, 0.17f, 0.4f));
+		counterweight2->move(glm::vec3(0.2f, 0.7f, 0.25f));
+
+		auto counterweight3 = std::unique_ptr<Cube>(new Cube(GREY));
+		counterweight3->scale(glm::vec3(1.4f, 0.17f, 0.4f));
+		counterweight3->move(glm::vec3(0.2f, 0.9f, 0.25f));
+
+		addObject(std::move(counterweight1));
+		addObject(std::move(counterweight2));
+		addObject(std::move(counterweight3));
+
 		this->rotate2(glm::vec3(0.0f, 0.0f, 90.0f));
 		this->move2(glm::vec3(8 * dbase, 10.0f, -dbase / 2));
+
 
 	}
 
