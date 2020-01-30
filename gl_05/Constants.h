c69683bd7add35471467454c2f1fe38c8673eb75
Maciej Kasprzyk <maciejkasprzyk98@gmail.com>
2020-01-30 06:18:30 +0100

    pierwsza wersja trzonu dzwigu

--- gl_05/Constants.h
+++ gl_05/Constants.h
@@ -6,5 +6,3 @@
 const GLuint WINDOW_WIDTH = 800, WINDOW_HEIGHT = 600;
 
 const glm::vec4 YELLOW = glm::vec4(0.96f, 0.89f, 0.3f, 1.0f);
-
-const float SQRT3OVER2 = 0.866;
--- gl_05/Crane.h
+++ gl_05/Crane.h
@@ -7,7 +7,7 @@
 #include "Compound.h"
 #include "Constants.h"
 #include <algorithm>
-
+#include <cmath>
 
 
 class Crane : public Compound {
@@ -15,21 +15,78 @@ public:
 
 	Crane() {
 
-		std::unique_ptr<Cube> base[3];
+		std::unique_ptr<Cube> stem[3]; // stem znaczy trzon
 		std::generate(
-			begin(base),
-			end(base),
+			begin(stem),
+			end(stem),
 			[]() { return std::make_unique<Cube>(YELLOW); }
 		);
 
-		for (auto& x : base) {
+		float thick = 0.05f;
+		float height = 10.0f;
 
-			x->scale(glm::vec3(0.05f, 2.0f, 0.05f));
-			x->move(glm::vec3(0.0f, 0.5f, 0.0f));
+		for (auto& x : stem) {
+
+			x->scale(glm::vec3(thick, height, thick));
+			x->move(glm::vec3(0.0f, height/2, 0.0f));
+		}
+		float dbase = 0.5f; // okresla jak bardzo oddalone sa od siebie dwie pierwsze rury trzonu
+
+		stem[1]->move(glm::vec3(0.0f, 0.0f, dbase));
+		stem[2]->move(glm::vec3(dbase * sqrt(3)/2, 0.0f, dbase/2));
+
+		for (auto& x : stem) {
+			addObject(std::move(x));
+		}
+		
+		// poprzeczki
+		const int n_bars = 10;
+		std::unique_ptr<Cube> bars[n_bars][3][2];
+		for (int i = 0; i < n_bars; i++) {
+			for (int j = 0; j < 3; j++) {
+				for (int k = 0; k < 2; k++) {
+					bars[i][j][k] = std::unique_ptr<Cube>(new Cube(YELLOW));
+				}
+			}
 		}
 
-		addObject(std::move(base[0]));
-		
+		float bar_thick = thick * 0.5;
+
+		for (int i = 0; i < n_bars; i++) {
+			for (int j = 0; j < 3; j++) {
+				for (int k = 0; k < 2; k++) {
+					bars[i][j][k]->scale(glm::vec3(bar_thick, dbase * sqrt(2.0f), bar_thick));
+					bars[i][j][k]->move(glm::vec3(0.0f, (2.0f * i + 0.5f) * dbase, dbase / 2.0f));
+					if (j == 1) {
+						bars[i][j][k]->move(glm::vec3(0.0f, 0.0f, -dbase / 2.0f));
+						bars[i][j][k]->rotate(glm::vec3(0.0f, 60.0f, 0.0f));
+						bars[i][j][k]->move(glm::vec3(0.0f, 0.0f, dbase / 2.0f));
+					}
+					else if (j == 2) {
+						bars[i][j][k]->move(glm::vec3(0.0f, 0.0f, dbase / 2.0f));
+						bars[i][j][k]->rotate(glm::vec3(0.0f, -60.0f, 0.0f));
+						bars[i][j][k]->move(glm::vec3(0.0f, 0.0f, -dbase / 2.0f));
+
+					}
+					if (k == 0) {
+						bars[i][j][k]->rotate(glm::vec3(45.0f, 0.0f, 0.0f)); 
+					}
+					else {
+						bars[i][j][k]->move(glm::vec3(0.0f, dbase, 0.0f));
+						bars[i][j][k]->rotate(glm::vec3(-45.0f, 0.0f, 0.0f));
+					}
+					
+				}
+			}
+		}
+
+		for (int i = 0; i < n_bars; i++) {
+			for (int j = 0; j < 3; j++) {
+				for (int k = 0; k < 2; k++) {
+					addObject(std::move(bars[i][j][k]));
+				}
+			}
+		}
 
 	}
 
